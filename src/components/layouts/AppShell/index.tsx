ff5c475571ee9c4ba5d32b4e190985f2895effb4
Nadhiradhit <nadhiradhitya@gmail.com>
2024-01-26 23:20:47 +0700

    feat: learn layouting next.js

---
+++ src/components/layouts/AppShell/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+export default function AppShell(props: any) {
+    const { children } = props
+    return (
+        <main>
+            <Navbar/>
+            {children}
+            <div>Footer</div>
+        </main>
+    )
+}
